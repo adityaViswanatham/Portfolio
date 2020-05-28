9ca3ff6effed03be212fd1fce31c0d0e0cad324a
av-2599 <a.r.viswanatham@gmail.com>
2020-05-28 16:33:22 -0500

    Added description

--- portfolio/src/component/Profile/profile.js
+++ portfolio/src/component/Profile/profile.js
@@ -17,15 +17,19 @@ export default ({ name }) => {
                 <Row>
                     <Col>
                         <Image className='icon-btn' src={ university } alt="university icon" />
+                        <p className='desc'><strong>Education</strong></p>
                     </Col>
                     <Col>
                         <Image className='icon-btn' src={ publication } alt="publication icon" />
+                        <p className='desc'><strong>Publications</strong></p>
                     </Col>
                     <Col>
                         <Image className='icon-btn' src={ research } alt="research icon" />
+                        <p className='desc'><strong>Research</strong></p>
                     </Col>
                     <Col>
                         <Image className='icon-btn' src={ students } alt="students icon" />
+                        <p className='desc'><strong>Students</strong></p>
                     </Col>
                 </Row>
             </div>
